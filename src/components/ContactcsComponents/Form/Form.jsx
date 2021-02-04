4beed4ff7d01c825bd7f01ec9a7a8902e1176445
Ignas Sungaila <Ignas.Sungaila@gmail.com>
2021-02-04 09:36:17 +0200

    email validation completed

--- src/components/ContactcsComponents/Form/Form.jsx
+++ src/components/ContactcsComponents/Form/Form.jsx
@@ -2,12 +2,16 @@ import React, {useState} from 'react'
 import IconButton from '@material-ui/core/IconButton';
 import SendIcon from '@material-ui/icons/Send';
 import emailjs from 'emailjs-com';
-import formInputs from '../../../static/formInputs'
+import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import './Form.css'
 
 
 function Form() {
     const [sent, setSent] = useState(false);
+
+    const [state, setState] = useState({
+                                email: ''
+    });
 
 
     function sendEmail(e) {
@@ -25,37 +29,67 @@ function Form() {
 
 
 return(
-    <form className="form" onSubmit={sendEmail} >
-        <h2 className="form-h2">Parašykite mums</h2>
 
-        {formInputs.map(input => (
-            <label key={input.label} id={input.id} className="form-label">
-                {input.label}
-
-                {input.type === 'textarea' ? (
-                    <textarea className="form-textarea" placeholder={input.placeholder} name={input.name}/>
-                ) : (
-                    <input
-                        className="form-input"
-                        type={input.type}
-                        placeholder={input.placeholder}
-                        name ={input.name}
-                    />
-                )}
-            </label>
-        ))}
+    <ValidatorForm
+        className="form"
+        onSubmit={sendEmail}
+        onError={errors => console.log(errors)}
+    >
+        <TextValidator
+            className="form-input"
+            id='email'
+            type='email'
+            label='Elektroninis paštas'
+            onChange={e => {
+                const newEmail = {email: e.target.value};
+                setState(newEmail);
+            }}
+            name={state.email}
+            value={state.email}
+            placeholder='tavo@elpastas.lt'
+            validators={['required', 'isEmail']}
+            errorMessages={['Šis laukas privalomas', 'Neteisingas el. paštas']}
+        />
 
         <IconButton type="submit" className="form-submit">
             <SendIcon style={{fontSize: 80}}/>
         </IconButton>
 
-    {sent === true ? (
-        <p className="messageSent"> Žinutė išsiūsta! </p>
-    ) : (<></>)}
+           {sent === true ? (
+             <p className="messageSent"> Žinutė išsiūsta! </p>
+             ) : (<></>)}
 
+    </ValidatorForm>
 
+    );}
 
-    </form>
-);}
+//     <form className="form" onSubmit={sendEmail} >
+//         <h2 className="form-h2">Parašykite mums</h2>
+//
+//         {formInputs.map(input => (
+//             <label key={input.label} id={input.id} className="form-label">
+//                 {input.label}
+//
+//                 {input.type === 'textarea' ? (
+//                     <textarea className="form-textarea" placeholder={input.placeholder} name={input.name}/>
+//                 ) : (
+//                     <input
+//                         className="form-input"
+//                         type={input.type}
+//                         placeholder={input.placeholder}
+//                         name ={input.name}
+//                     />
+//                 )}
+//             </label>
+//         ))}
+//
+//
+//
+
+//
+//
+//
+//     </form>
+// );}
 
 export default Form
