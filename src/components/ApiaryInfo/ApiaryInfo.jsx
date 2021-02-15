f5e1140ecf4bee3754edcf1d8dee05a950e39610
Ignas Sungaila <sungi@pop-os.localdomain>
2021-02-15 05:53:26 +0200

    apiary page done

--- src/components/ApiaryInfo/ApiaryInfo.jsx
+++ src/components/ApiaryInfo/ApiaryInfo.jsx
@@ -4,6 +4,8 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import papile from '../../assets/papile.png';
+import papileview from '../../assets/papileview.JPG';
+import apiaryinfo from '../../assets/apiaryinfo.jpg';
 
 
 
@@ -18,11 +20,18 @@ const useStyles = makeStyles({
         fontFamily: 'cursive',
 
     },
-    image:{
+    imagelogo:{
         marginTop: '20px',
         width:'250px',
         height: '150px',
         paddingRight: '50px',
+
+    },
+    image:{
+        marginTop: '20px',
+        width:'300px',
+        height: '250px',
+        padding: '5px',
 
     },
     paper:{
@@ -39,17 +48,44 @@ export default function ApiaryInfo(){
               direction="row"
               justify="space-between"
               alignItems="center">
-            <Grid item xs={9}>
+
+            <Grid item xs={10}>
                 <Typography className={classes.text}>
                 Mūsų bitynas yra įkurtas įspūdingo grožio ir pilno gyvos gamtos Papilės
                     miestelyje, kuris yra Ventos regioniniame parke.
                 </Typography>
             </Grid>
-            <Grid item xs={3} color='transparent'>
+            <Grid item xs={2} color='transparent'>
                 <Paper className={classes.paper} elevation={0}>
-                    <img className={classes.image} src={papile} alt="papile"/>
+                    <img className={classes.imagelogo} src={papile} alt="papile"/>
                 </Paper>
             </Grid>
+
+
+            <Grid item xs={3} color='transparent'>
+                <Paper className={classes.paper} elevation={0}>
+                    <img className={classes.image} src={papileview} alt="papile"/>
+                </Paper>
+            </Grid>
+            <Grid item xs={8}>
+                <Typography className={classes.text}>
+                    Nešamas medus yra iš gausios gamtos Ventos paupio. Visiškai natūralus, saldus ir sveikas pavasarinis medus,
+                     bei neišpasakytasis vasaros Liepų medus.
+                </Typography>
+            </Grid>
+
+            <Grid item xs={9}>
+                <Typography className={classes.text}>
+                    Kruopščiai aprūpintą ir prižiūrimą bityną sudaro 30 šeimų. Mūsų gaminami, bei tiekiami, produktai yra
+                    100% natūralus. Taip pat kuriame mielas dovanėles tam tikroms šventinėmis progomis.
+                </Typography>
+            </Grid>
+            <Grid item xs={3} color='transparent'>
+                <Paper className={classes.paper} elevation={0}>
+                    <img className={classes.image} src={apiaryinfo} alt="papile"/>
+                </Paper>
+            </Grid>
+
         </Grid>
     );
 }
